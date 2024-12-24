5875647c67e3ecd5b300f9f4e15d875c24ec54ec
unknown <forkan.saadaoui@gmail.com>
2024-12-24 07:35:09 +0100

    update-portfolio

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -3,13 +3,13 @@ import RightIcons from "./RightIcons";
 import { FaLinkedinIn } from "react-icons/fa6";
 import { FaYoutube, FaGithub } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
-import { MdEmail } from "react-icons/md";
+// import { MdEmail } from "react-icons/md";
 
 const Main = () => {
   return (
     <div>
-      <h2 className="fixed top-2 md:top-9 left-9 md:left-20 font-unlock text-sm md:text-md text-dark dark:text-white">
-        <a href="mailto:contact@saadaouidev.com">contact@saadaouidev.com</a>
+      <h2 className="fixed top-2 md:top-9 left-9 md:left-20 font-unlock text-sm md:text-lg text-dark dark:text-white">
+        <a href="mailto:contact.saadaouimahmoud@gmail.com">contact.saadaouimahmoud@gmail.com</a>
       </h2>
 
       <div className="fixed bottom-16 md:bottom-20 left-3 md:left-9 flex flex-col space-y-4 text-xl text-text_light dark:text-text_dark">
@@ -17,7 +17,7 @@ const Main = () => {
         <a href="https://www.youtube.com/@mahmoud_saadaoui" target="_blanc"><FaYoutube/></a>
         <a href="https://github.com/Saadaoui-Forkan" target="_blanc"><FaGithub/></a>
         <a href="https://wa.me/21627987081" target="_blanc"><IoLogoWhatsapp/></a>
-        <a href="mailto:saadaouidev@gmail.com"><MdEmail/></a>
+        {/* <a href="mailto:saadaouidev@gmail.com"><MdEmail/></a> */}
       </div>
 
       <RightIcons />
