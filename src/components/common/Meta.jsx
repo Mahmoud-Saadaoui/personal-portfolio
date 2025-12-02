fbc7dbe567af019b5cc16b54e4fc614f54bdffe7
Mahmoud-Saadaoui <forkan.saadaoui@gmail.com>
2025-12-02 11:18:42 +0100

    fix: Document does not have a meta description

--- src/components/common/Meta.jsx
+++ src/components/common/Meta.jsx
@@ -5,7 +5,17 @@ const Meta = ({ title, description, keywords }) => {
     <Helmet>
       <title>{title}</title>
       <meta name='description' content={description} />
-      <meta name='keyword' content={keywords} />
+      <meta name='keywords' content={keywords} />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
     </Helmet>
   );
 };
